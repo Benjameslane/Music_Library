3c578f927cb7631f9259384310374cc6a20c9429
Benjameslane <ben.james.lane9>
2023-07-10 16:57:16 -0600

    test repo

---
+++ MusicLibraryWebAPI/Controllers/MusicsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicLibraryWebAPI.Data;
+using MusicLibraryWebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicLibraryWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicsController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public MusicsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<MusicsController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var musics = _context.Musics.ToList();
+            return Ok(musics);
+        }
+
+        // GET api/<MusicsController>/5
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MusicsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Music music)
+        {
+            _context.Musics.Add(music);
+            _context.SaveChanges();
+            return StatusCode(201, music);
+        }
+
+        // PUT api/<MusicsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MusicsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MusicLibraryWebAPI/Models/Music.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicLibraryWebAPI.Models
+{
+    public class Music
+    {
+
+        // do one of these need to be taken out due to them being part of a collection?
+
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist{ get; set; }
+        public string Album{ get; set; }
+        public int ReleaseDate{ get; set; }
+        public string Genre { get; set; }
+    }
+}
